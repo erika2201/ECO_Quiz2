21fc24f245dfb8d65b962c73df807b28e830dd32
erika2201 <erikapapamija@gmail.com>
2021-11-04 19:33:30 -0500

    no se que pasa con las tarjetas

--- src/index.js
+++ src/index.js
@@ -39,6 +39,22 @@ function getCourses(){
 //Aquí sucede la magia
 function currentList(info){
     if(info){
+        //Todo esto sirve para pasar las tarjetas entre sì
+        noBonus.innerHTML = "";
+        let sinBono=document.createElement("h2");
+        sinBono.innerHTML="Sin Bono"
+        noBonus.appendChild(sinBono);
+
+        silverBonus.innerHTML="";
+        let bonoPlata=document.createElement("h2");
+        bonoPlata.innerHTML="Bono plata +0,3";
+        silverBonus.appendChild(bonoPlata);
+
+        goldBonus.innerHTML="";
+        let bonoOro=document.createElement("h2");
+        bonoOro.innerHTML="Bono oro +0,5";
+        goldBonus.appendChild(bonoOro);
+
         //Me da el arreglo de las llaves de un objeto
         Object.keys(info).forEach((k,index)=>{
             console.log(k, index);
@@ -56,6 +72,25 @@ function currentList(info){
         });
             
     }else{
+        let regNoBonus=document.createElement("p");
+        regNoBonus.innerHTML="";
+        let regBonusPlata=document.createElement("p");
+        regBonusPlata.innerHTML="";
+        let regBonusOro=document.createElement("p");
+        regBonusOro.innerHTML="";
+
+        noBonus.innerHTML="";
+        noBonus.appendChild(sinBono);
+        noBonus.appendChild(regNoBonus);
+
+
+        silverBonus.innerHTML="";
+        silverBonus.appendChild(bonoPlata);
+        silverBonus.appendChild(regBonusPlata);
+
+        goldBonus.innerHTML="";
+        goldBonus.appendChild(bonoOro);
+        goldBonus.appendChild(regBonusOro);
 
     }
     
